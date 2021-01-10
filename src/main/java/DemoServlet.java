e78851849a5e8fa6e142e0048a597851de68ef25
gopu <ppoorg2021@gmail.com>
2021-01-10 14:17:59 +0530

    Initial commit

---
+++ src/main/java/DemoServlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/DemoServlet")
+public class DemoServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h3>Hello World!</h3>");
+
+        int i = Integer.parseInt(request.getParameter("t1"));
+        int j = Integer.parseInt(request.getParameter("t2"));
+        int k = i + j;
+
+        out.println(k);
+
+    }
+}
